24d54ee035959b0eaf49a9c9b868cf64a2c6b736
Josh Cooter <thejoshcooter@gmail.com>
2020-03-03 10:37:20 -0600

    add standard css reset via global style injection

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom';
 // router
 import {BrowserRouter as Router} from 'react-router-dom';
 // global themes and style injections
+import { Reset } from './styles/Reset';
 import { GlobalStyles } from './styles/Global';
 // components
 import App from './App';
 
 ReactDOM.render(
     <Router>
+        <Reset />
         <GlobalStyles />
         <App />
     </Router>, 
