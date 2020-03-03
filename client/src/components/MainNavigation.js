6228a64fe92e636a066a75705cb381d81f6ec235
Josh Cooter <thejoshcooter@gmail.com>
2020-03-03 12:11:51 -0600

    stub out  MainNavigation component and import to Landing index

---
+++ client/src/components/MainNavigation.js
@@ -0,0 +1,18 @@
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+
+// component
+const MainNavigation = () => {
+    return (
+        <>
+        hi from main navigation
+        </>
+    );
+};
+
+// styled components
+
+// export
+export default MainNavigation;
--- client/src/views/landing/index.js
+++ client/src/views/landing/index.js
@@ -4,15 +4,21 @@ import React from 'react';
 // todo
 // styled components
 import styled from 'styled-components';
+// components
+import MainNavigation from '../../components/MainNavigation'
 
 // component
 const Landing = () => {
     return (
         <>
+        <MainNavigation />
         hi from landing page.
         </>
     );
 };
+
+// styled components
+// todo
 
 // export
 export default Landing;
