4fcd640bc06f09f0e62f133ef93e7c83ad8b7d4f
Josh Cooter <thejoshcooter@gmail.com>
2020-03-03 10:33:30 -0600

    add styles directory for global themes and style injections. add basic GlobalStyles.js and verify theming functionality after importing to index.js.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // router
 import {BrowserRouter as Router} from 'react-router-dom';
+// global themes and style injections
+import { GlobalStyles } from './styles/Global';
 // components
 import App from './App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <GlobalStyles />
+        <App />
+    </Router>, 
+    document.getElementById('root'));
