6082d2521a136aaeb7d7565408b555ca046c2459
Josh Cooter <thejoshcooter@gmail.com>
2020-03-04 09:42:58 -0600

    add default letter spacing of 0.015rem to default theme.

--- client/src/styles/Global.js
+++ client/src/styles/Global.js
@@ -7,5 +7,6 @@ export const GlobalStyles = createGlobalStyle`
         font-size: 1rem;
         background-color: #1F2933;
         font-family: 'Roboto', sans-serif;
+        letter-spacing: 0.015rem;
     }
 `
