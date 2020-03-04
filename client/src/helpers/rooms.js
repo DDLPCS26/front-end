cb5e4fe139d46eb01cb3f9b9e0251c5903edf39f
Josh Cooter <thejoshcooter@gmail.com>
2020-03-04 17:00:19 -0600

    wrote rooms.js dummy data to test visualization methods

---
+++ client/src/helpers/rooms.js
@@ -0,0 +1,230 @@
+// dummy data to test room visualization
+
+export const rooms = [
+    {
+        room_id: 1,
+        north: false,
+        south: false,
+        east: true,
+        west: false,
+        title: "first",
+        description: "first room"
+    }
+    {
+        room_id: 2,
+        north: false,
+        south: true,
+        east: false,
+        west: true,
+        title: "second",
+        description: "second room"
+    }
+    {
+        room_id: 3,
+        north: true,
+        south: true,
+        east: true,
+        west: false,
+        title: "third",
+        description: "third room"
+    }
+    {
+        room_id: 4,
+        north: false,
+        south: true,
+        east: false,
+        west: true,
+        title: "fourth",
+        description: "fourth room"
+    }
+    {
+        room_id: 5,
+        north: false,
+        south: false,
+        east: false,
+        west: true,
+        title: "fifth",
+        description: "fifth room"
+    }
+    {
+        room_id: 6,
+        north: false,
+        south: true,
+        east: true,
+        west: false,
+        title: "sixth",
+        description: "sixth room"
+    }
+    {
+        room_id: 7,
+        north: true,
+        south: false,
+        east: false,
+        west: true,
+        title: "seventh",
+        description: "seventh room"
+    }
+    {
+        room_id: 8,
+        north: true,
+        south: false,
+        east: true,
+        west: false,
+        title: "eigth",
+        description: "eigth room"
+    }
+    {
+        room_id: 9,
+        north: false,
+        south: true,
+        east: false,
+        west: true,
+        title: "ninth",
+        description: "ninth room"
+    }
+    {
+        room_id: 10,
+        north: false,
+        south: true,
+        east: false,
+        west: true,
+        title: "tenth",
+        description: "tenth room"
+    }
+    {
+        room_id: 11,
+        north: true,
+        south: false,
+        east: true,
+        west: false,
+        title: "eleventh",
+        description: "eleventh room"
+    }
+    {
+        room_id: 12,
+        north: false,
+        south: true,
+        east: true,
+        west: true,
+        title: "twelth",
+        description: "twelth room"
+    }
+    {
+        room_id: 13,
+        north: false,
+        south: true,
+        east: false,
+        west: true,
+        title: "thirteenth",
+        description: "thirteenth room"
+    }
+    {
+        room_id: 14,
+        north: true,
+        south: true,
+        east: false,
+        west: false,
+        title: "fourteenth",
+        description: "fourteenth room"
+    }
+    {
+        room_id: 15,
+        north: true,
+        south: false,
+        east: false,
+        west: false,
+        title: "fifteenth",
+        description: "fifteenth room"
+    }
+    {
+        room_id: 16,
+        north: false,
+        south: true,
+        east: true,
+        west: false,
+        title: "sixteenth",
+        description: "sixteenth room"
+    }
+    {
+        room_id: 17,
+        north: true,
+        south: false,
+        east: false,
+        west: true,
+        title: "seventeenth",
+        description: "seventeenth room"
+    }
+    {
+        room_id: 18,
+        north: true,
+        south: false,
+        east: true,
+        west: false,
+        title: "eighteenth",
+        description: "eighteenth room"
+    }
+    {
+        room_id: 19,
+        north: true,
+        south: false,
+        east: false,
+        west: true,
+        title: "nineteenth",
+        description: "nineteenth room"
+    }
+    {
+        room_id: 20,
+        north: false,
+        south: true,
+        east: false,
+        west: true,
+        title: "twentieth",
+        description: "twentieth room"
+    }
+    {
+        room_id: 21,
+        north: true,
+        south: false,
+        east: true,
+        west: false,
+        title: "twenty-first",
+        description: "twenty-first room"
+    }
+    {
+        room_id: 22,
+        north: false,
+        south: false,
+        east: true,
+        west: true,
+        title: "twenty-second",
+        description: "twenty-second room"
+    }
+    {
+        room_id: 23,
+        north: false,
+        south: true,
+        east: false,
+        west: true,
+        title: "twenty-third",
+        description: "twenty-third room"
+    }
+    {
+        room_id: 24,
+        north: false,
+        south: false,
+        east: true,
+        west: false,
+        title: "twenty-fourth",
+        description: "twenty-fourth room"
+    }
+    {
+        room_id: 25,
+        north: true,
+        south: false,
+        east: false,
+        west: true,
+        title: "twenty-fifth",
+        description: "twenty-fifth room"
+    }
+];
+
