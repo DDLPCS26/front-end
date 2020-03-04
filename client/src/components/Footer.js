7ce924b71da9170b93b287882a7bfa6744f925e8
Josh Cooter <thejoshcooter@gmail.com>
2020-03-04 12:54:15 -0600

    stub out footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,20 @@
+// react
+import React from 'react';
+// styled components
+import styled from 'styled-components';
+// helpers
+import { styletest } from '../helpers/styletest';
+
+// component
+const Footer = props => {
+    return (
+        <>
+        hi from footer
+        </>
+    );
+};
+
+// styled components
+// todo
+
+export default Footer;
