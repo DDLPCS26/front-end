cd936cc018928c6d4994ee7a916739f9e55ba7fc
Josh Cooter <thejoshcooter@gmail.com>
2020-03-05 12:44:04 -0600

    add dummy player data

---
+++ client/src/helpers/player.js
@@ -0,0 +1,17 @@
+// player init dummy data
+
+export const dummyPlayer = {
+    uuid: "",
+    name: "tester",
+    room_id: 1,
+    title: "Outside Cave Entrance",
+    description: "North of you, the cave mount beckons",
+    players: [
+        "ian",
+        "newuser",
+        "higuys",
+        "mattherich"
+    ],
+    grid_x: 0,
+    grid_y: 0
+};
