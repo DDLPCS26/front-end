929d050239f8164329473ab187320ab50b8be47f
Josh Cooter <thejoshcooter@gmail.com>
2020-03-06 11:46:20 -0600

    remove  old dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import Footer from './components/Footer';
 import Landing from './views/landing';
 import Login from './views/user/Login';
 import Register from './views/user/Register'
-import GameDashboard from './views/game/index';
 import Sandbox from './views/Sandbox';
 
 const App = props => {
@@ -21,7 +20,7 @@ const App = props => {
       <Route exact path='/register' component={Register} />
       {/* the game dashboard needs to move to a private route in production */}
       {/* <Route path='/' component={Footer} /> */}
-      <Route exact path='/sandbox' component={Sandbox} />
+      <Route exact path='/dashboard' component={Sandbox} />
     </>
   );
 }
