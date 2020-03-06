e89af6097f23d54e70e08ed9cd583e02b732f0af
Josh Cooter <thejoshcooter@gmail.com>
2020-03-06 12:01:50 -0600

    setup registration form to send to dashboard

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,7 @@
 // react
 import React, { useState, useContext } from 'react';
+// router
+import { useHistory } from 'react-router-dom';
 // styled components
 import styled from 'styled-components';
 // helpers
@@ -9,6 +11,9 @@ import { DefaultTheme } from '../contexts/DefaultTheme';
 
 const Login = props => {
   const [login, setLogin] = useState({username: "", password: ""})
+  const history = useHistory();
+
+  console.log('checking login history:', history);
 
   // contexts
   const colorPalette = useContext(DefaultTheme).colors;
@@ -18,15 +23,17 @@ const Login = props => {
   }
   const handleSubmit = e => {
     e.preventDefault();
+    history.push('/dashboard');
   }
+
   return (
     <Container colors={colorPalette}>
-      <Form action={handleSubmit}>
+      <Form onSubmit={handleSubmit}>
           <Label for="uname">Username</Label>
-          <Input type="text" required id="uname" name="username" value={login.username} onChange={handleChange} colors={colorPalette}/>
+          <Input type="text" id="uname" name="username" value={login.username} onChange={handleChange} colors={colorPalette}/>
           
           <Label for="pword">Password</Label>
-          <Input type="password" required id="pword" name="password" value={login.password} onChange={handleChange} colors={colorPalette}/>
+          <Input type="password" id="pword" name="password" value={login.password} onChange={handleChange} colors={colorPalette}/>
   
           <Button type="submit" colors={colorPalette}>Submit</Button>
       </Form>
